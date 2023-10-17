68230ef69b930c95329cc630e5bf70b00fc0432d
google0101-ryan <ryan.ilari@stu.scott.kyschools.us>
2023-10-17 17:00:35 -0400

    Changed catapult functionality

--- src/Competition2023.cpp
+++ src/Competition2023.cpp
@@ -15,6 +15,7 @@ private:
 
     pros::Motor* left[3], *right[3];
     pros::Motor* catapult;
+    pros::ADIDigitalIn* limitSwitch;
 public:
     virtual void Initialize();
     virtual void DoAutonomous();
@@ -33,44 +34,26 @@ void Competition2023::Initialize()
     lcd->Initialize();
     lcd->AddErrorText("Test text: 0x%x", 0xAAAAAAAA);
 
-    // vision = new pros::Vision(VISION_SYSTEM_PORT);
-    // if (errno == PROS_ERR)
-    // {
-    //     Error("Cannot configure vision system on port %d: %s\n", VISION_SYSTEM_PORT, strerror(errno));
-    // }
+    vision = new pros::Vision(VISION_SYSTEM_PORT);
+    if (errno == PROS_ERR)
+    {
+        Error("Cannot configure vision system on port %d: %s\n", VISION_SYSTEM_PORT, strerror(errno));
+    }
 
-    // left[0] = new pros::Motor(1);
-    // left[1] = new pros::Motor(2);
-    // left[2] = new pros::Motor(3);
-    // left[2]->set_reversed(true);
-    // right[0] = new pros::Motor(4);
-    // right[0]->set_reversed(true);
-    // right[1] = new pros::Motor(5);
-    // right[1]->set_reversed(true);
-    // right[2] = new pros::Motor(6);
-    catapult = new pros::Motor(9, true);
+    left[0] = new pros::Motor(1);
+    left[1] = new pros::Motor(2);
+    left[2] = new pros::Motor(3);
+    left[2]->set_reversed(true);
+    right[0] = new pros::Motor(4);
+    right[0]->set_reversed(true);
+    right[1] = new pros::Motor(5);
+    right[1]->set_reversed(true);
+    right[2] = new pros::Motor(6);
+    catapult = new pros::Motor(14, MOTOR_GEARSET_36, true);
     catapult->set_brake_mode(pros::motor_brake_mode_e_t::E_MOTOR_BRAKE_HOLD);
     catapult->move_absolute(0.0, 100);
 
-    printf("%d\n", errno);
-
-    // vision::signature GREEN_TRIANGLE (1, -5031, -4343, -4687, -5091, -3925, -4508, 8.600, 0);
-
-    // auto g_triangle = vision->signature_from_utility(1, -5031, -4343, -4687, -5091, -3925, -4508, 8.600, 0);
-    // vision->set_signature(1, &g_triangle);
-    
-    // auto g_triangle2 = vision->signature_from_utility(2, -4969, -3855, -4412, -4805, -3449, -4127, 5.700, 0);
-    // vision->set_signature(2, &g_triangle);
-
-    // printf("%d\n", vision->get_object_count());
-
-    // auto nearest_object = vision->get_by_size(0);
-    // printf("%d\n", nearest_object.signature);
-
-    // if (nearest_object.signature == 255)
-    // {
-    //     printf("%d\n", errno);
-    // }
+    limitSwitch = new pros::ADIDigitalIn('E');
 }
 
 void Competition2023::DoAutonomous()
@@ -80,6 +63,8 @@ void Competition2023::DoAutonomous()
         pros::delay(2);
     }
 }
+
+bool catapultIsMoving = false;
 
 void Competition2023::DoOpControl()
 {
@@ -92,17 +77,21 @@ void Competition2023::DoOpControl()
         int left_ = fwd - turn;
         int right_ = fwd + turn;
 
-        // for (int i = 0; i < 3; i++)
-        //     left[i]->move(left_);
-        // for (int i = 0; i < 3; i++)
-        //     right[i]->move(right_);
+        for (int i = 0; i < 3; i++)
+            left[i]->move(left_);
+        for (int i = 0; i < 3; i++)
+            right[i]->move(right_);
 
-        if (controller->get_digital(DIGITAL_A))
+        if (catapultIsMoving && limitSwitch->get_value())
+        {
+            catapult->brake();
+            catapultIsMoving = false;
+        }
+
+        if (controller->get_digital(DIGITAL_A) && !catapultIsMoving)
         {
             catapult->move_velocity(100);
-            pros::delay(800);
-            catapult->brake();
-            printf("%d\n", catapult->get_position());
+            catapultIsMoving = true;
         }
 
         pros::delay(2);
